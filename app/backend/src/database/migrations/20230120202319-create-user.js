f32a81e6cfd21bf1836d4e7a37d9e03a46cfb546
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-01-20 17:43:23 -0300

    Arquivos Dockerfile criados, iniciando elaboração das migrations.

---
+++ app/backend/src/database/migrations/20230120202319-create-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('users');
+  },
+};
